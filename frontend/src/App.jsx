688ae9501f7bfebe09ede6e5e7e6d8d8426fbb87
Mohammad Ahmad <mohammad@authornate.com>
2024-12-16 10:56:16 +0500

    Added shadcn/ui and other pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Data from "./pages/Data";
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <div className="flex flex-col justify-center items-center py-20 gap-10 px-4 md:px-8">
+      <h1 className="text-blue font-bold text-center text-7xl max-md:text-5xl">
+        Full Stack Assessment
+      </h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Data />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
